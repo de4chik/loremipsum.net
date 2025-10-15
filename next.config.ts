59d3ca57b2aa253f8e2e52f540074571c800d3df
eugene <hom.1232015@yandex.ru>
2025-10-15 11:49:47 +0200

    update next config

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,11 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+	output: "export",
+	// при необходимости добавь:
+	images: {
+		unoptimized: true, // нужно для GitHub Pages
+	},
+};
 
 export default nextConfig;
